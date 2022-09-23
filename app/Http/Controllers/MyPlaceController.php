1adb9d4f3da797630e614f909db4602fb5bf0baa
Pulat Y <velikiy7man@gmail.com>
2022-09-23 13:03:13 +0500

    init laravel

---
+++ app/Http/Controllers/MyPlaceController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyPlaceController extends Controller
+{
+    public function index()
+    {
+        return "This page from MyPlaceController";
+    }
+    public function clip()
+    {
+        return "aaaaaaaaaaaaaaa";
+    }
+}
