c6814bf64d0331d16d80d4b04c24badf81d443a9
Pulat Y <velikiy7man@gmail.com>
2022-09-30 18:07:36 +0500

    change

--- database/migrations/2022_09_30_125900_add_column_text_to_posts_table.php
+++ database/migrations/2022_09_30_125900_add_column_text_to_posts_table.php
@@ -14,7 +14,7 @@ class AddColumnTextToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->text('text')->nullable();
+            $table->text('text')->nullable()->after('description');
         });
     }
 
